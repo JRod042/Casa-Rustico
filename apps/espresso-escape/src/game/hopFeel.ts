e344885fe33fcf3f195f64994189c64cf39d82f6
JRod042 <jorge.k.rodriguezvargas@gmail.com>
2026-09-07 17:10:28 -0500

    feat(espresso-escape): hop constants

---
+++ apps/espresso-escape/src/game/hopFeel.ts
@@ -0,0 +1,6 @@
+export const LBP_JUMP_V = -820;
+export const LBP_GRAVITY_UP = 1950;
+export const LBP_GRAVITY_DOWN = 3400;
+export const LBP_GRAVITY_HANG = 1550;
+export const LBP_PLAYER_W = 52;
+export const LBP_PLAYER_H = 66;
